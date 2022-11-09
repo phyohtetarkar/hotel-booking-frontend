d1cd2883863e8825e570b68a0648a69625949b7c
Theinmwe Naing <theinmwenaing22@gmail.com>
2022-11-09 22:04:11 +0630

    update hotel detail

--- src/pages/hotels/[slug].tsx
+++ src/pages/hotels/[slug].tsx
@@ -1,23 +1,20 @@
+import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import {
   CalendarDaysIcon,
   MapPinIcon,
   MinusIcon,
   PlusIcon,
-  UserIcon,
+  UserGroupIcon,
   WifiIcon,
 } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import { useState } from "react";
 import { Calendar } from "react-calendar";
 import { formControlHeight } from "../../common/app.config";
-import { Input } from "../../components/forms";
+import { Input, Select } from "../../components/forms";
 import Popover from "../../components/Popover";
 
 function HotelDetail() {
-  let image = `https://source.unsplash.com/random/200x240?random=${Math.floor(
-    Math.random() * 100
-  )}`;
-
   const [checkInDate, setCheckInDate] = useState(new Date());
   const [checkOutDate, setCheckOutDate] = useState(new Date());
 
@@ -116,11 +113,6 @@ function HotelDetail() {
                       </div>
                     </div>
                     <div className="col-12 col-lg">
-                      {/* <Input
-                    label="Room &amp; guests"
-                    value="2 adults - 0 children - 1 room"
-                    readonly
-                  /> */}
                       <Popover
                         renderReferenceElement={(handleClick) => {
                           return (
@@ -241,27 +233,35 @@ function HotelDetail() {
           </div>
         </div>
       </div>
+
       <div className="container py-3">
-        <div className="col-lg-12">
-          <h2>Ostello Bello Bagan</h2>
-          <div className="hstack">
-            <MapPinIcon width={20} />
-            <span className="ms-2 fw-medium">
-              Hkan Latt Quarter, Main Road New Bagan, Myanmar
-            </span>
+        <div className="card">
+          <div className="card-body">
+            <div className="hstack">
+              <h2 className="flex-grow-1">Ostello Bello Bagan</h2>
+              <div className="mt-1">
+                <small className="small text-muted">Starting from</small>
+                <div className="hstack align-items-baseline">
+                  <h4 className="mb-0">MMK 10,000</h4>
+                  <small className="text-muted ms-1">/ per night</small>
+                </div>
+              </div>
+            </div>
+
+            <div className="hstack gap-1 text-muted">
+              <MapPinIcon width={20} />
+              <span>Hkan Latt Quarter, Main Road New Bagan, Myanmar</span>
+            </div>
           </div>
         </div>
       </div>
+
       <div className="container">
         <div className="row g-3">
           <div className="col-lg-8">
-            <div
-              className="ratio ratio-4x3 d-lg-block"
-              style={{ maxWidth: 900, maxHeight: 600 }}
-            >
+            <div className="ratio ratio-16x9 d-lg-block">
               <Image
                 src="/images/bagan.jpeg"
-                className="rounded-3"
                 alt=""
                 fill
                 style={{ objectFit: "cover" }}
@@ -269,8 +269,8 @@ function HotelDetail() {
             </div>
           </div>
           <div className="col-lg-4">
-            <div className="vstack gap-2">
-              <div className="card rounded-3">
+            <div className="vstack gap-3 h-100">
+              <div className="card h-100">
                 <div className="card-header bg-white">
                   <h5 className="mt-2"> Most Popular Facilities</h5>
                 </div>
@@ -315,70 +315,228 @@ function HotelDetail() {
                   </div>
                 </div>
               </div>
-              <div className="card rounded-3">
+              <div className="card h-100">
                 <div className="card-body">View on map</div>
               </div>
             </div>
           </div>
         </div>
-        <div className="card rounded-3 mt-3">
-          <div className="card-header bg-white">
-            <h5 className="mt-2"> Recommand for 3 adults</h5>
-          </div>
-          <div className="card-body">
-            <div className="vstack gap-2">
-              <div className="hstack gap-2">
-                <div className="flex-grow-1">
-                  3 x{" "}
-                  <a href="#" style={{ color: "blue" }}>
-                    Bed in 10-Bed Mixed Dormitory Room
-                  </a>
-                </div>
-                <div className="vstack gap-2">
-                  <div className="hstack align-items-end">
-                    <div>
-                      <small className="small text-muted">Starting from</small>
-                      <div className="hstack align-items-baseline">
-                        <h4 className="mb-0">10,000</h4>
-                        <small className="text-muted ms-1">/ per night</small>
+
+        <div className="row g-3 py-3">
+          <div className="col-lg-8">
+            <div className="card h-100">
+              <div className="card-header bg-white">
+                <h5 className="mt-2"> Available Room Type</h5>
+              </div>
+              <div className="card-body">
+                <div className="row g-3">
+                  <div className="col-lg-6">
+                    <div className="vstack gap-2">
+                      <h5
+                        className="fw-medium text-decoration-underline"
+                        style={{ color: "blue" }}
+                      >
+                        Standard Room
+                      </h5>
+                      <div className="hstack gap-3">
+                        <div className="hstack gap-2">
+                          <span>2 twin beds</span>
+                          <CheckCircleIcon width={25} />
+                        </div>
+                        <div className="hstack gap-2">
+                          <span>2 x adults</span>
+                          <UserGroupIcon width={25} />
+                        </div>
+                        <div className="hstack gap-2">
+                          <span>1 x child</span>
+                          <CheckCircleIcon width={25} />
+                        </div>
+                      </div>
+                      <div className="mt-3">
+                        <span
+                          className="small fw-bold"
+                          style={{ color: "blue" }}
+                        >
+                          View detail
+                        </span>
                       </div>
                     </div>
-                    <div className="flex-grow-1"></div>
-                    <button className="btn btn-primary">Reserve</button>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Price for 3 nights</h5>
+                      <h5 className="fw-light">MMK 50,000</h5>
+                      <h5 className="small text-danger mt-3">
+                        * Only 3 rooms left on our site
+                      </h5>
+                    </div>
+                  </div>
+                  <div className="col-lg-2">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Select room</h5>
+                      <Select>
+                        <option>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                      </Select>
+                    </div>
+                  </div>
+
+                  <hr className="bg-dark-gray my-4" />
+                </div>
+
+                <div className="row g-3">
+                  <div className="col-lg-6">
+                    <div className="vstack gap-2">
+                      <h5
+                        className="fw-medium text-decoration-underline"
+                        style={{ color: "blue" }}
+                      >
+                        Deluxe Room
+                      </h5>
+                      <div className="hstack gap-3">
+                        <div className="hstack gap-2">
+                          <span>1 double bed</span>
+                          <CheckCircleIcon width={25} />
+                        </div>
+                        <div className="hstack gap-2">
+                          <span>2 x adults</span>
+                          <UserGroupIcon width={25} />
+                        </div>
+                      </div>
+                      <div className="mt-3">
+                        <span
+                          className="small fw-bold"
+                          style={{ color: "blue" }}
+                        >
+                          View detail
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Price for 3 nights</h5>
+                      <h5 className="fw-light">MMK 100,000</h5>
+                      <h5 className="small text-danger mt-3">
+                        * Only 3 rooms left on our site
+                      </h5>
+                    </div>
+                  </div>
+                  <div className="col-lg-2">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Select room</h5>
+                      <Select>
+                        <option>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                      </Select>
+                    </div>
+                  </div>
+
+                  <hr className="bg-dark-gray my-4" />
+                </div>
+
+                <div className="row g-3">
+                  <div className="col-lg-6">
+                    <div className="vstack gap-2">
+                      <h5
+                        className="fw-medium text-decoration-underline"
+                        style={{ color: "blue" }}
+                      >
+                        Premier Deluxe Room
+                      </h5>
+                      <div className="hstack gap-3">
+                        <div className="hstack gap-2">
+                          <span>2 twin beds</span>
+                          <CheckCircleIcon width={25} />
+                        </div>
+                        <div className="hstack gap-2">
+                          <span>2 x adults</span>
+                          <UserGroupIcon width={25} />
+                        </div>
+                        <div className="hstack gap-2">
+                          <span>1 x child</span>
+                          <CheckCircleIcon width={25} />
+                        </div>
+                      </div>
+                      <div className="mt-3">
+                        <span
+                          className="small fw-bold"
+                          style={{ color: "blue" }}
+                        >
+                          View detail
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-lg-4">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Price for 3 nights</h5>
+                      <h5 className="fw-light">MMK 200,000</h5>
+                      <h5 className="small text-danger mt-3">
+                        * Only 1 room left on our site
+                      </h5>
+                    </div>
+                  </div>
+                  <div className="col-lg-2">
+                    <div className="vstack gap-2">
+                      <h5 className="fw-medium">Select room</h5>
+                      <Select>
+                        <option>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                      </Select>
+                    </div>
                   </div>
                 </div>
               </div>
-              <div className="hstack gap-2 fw-medium">
-                Price for :
-                <UserIcon width={20} />
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card h-100">
+              <div className="card-header bg-white">
+                <h5 className="mt-2"> Select Room</h5>
               </div>
-              <div className="hstack gap-2 fw-bold" style={{ color: "green" }}>
-                Free cancellation before 10 Dec 2022
-              </div>
-              <div className="hstack gap-2 fw-bold" style={{ color: "green" }}>
-                Breakfast included
-              </div>
+              <div className="card-body"></div>
             </div>
           </div>
         </div>
-        <div className="card rounded-3 mt-3">
-          <div className="card-header bg-white">
-            <h5 className="mt-2">Description</h5>
+      </div>
+
+      <div className="container">
+        <div className="row g-3">
+          <div className="col-lg-12">
+            <div className="card">
+              <div className="card-header bg-white">
+                <h5 className="mt-2"> Hotel Description</h5>
+              </div>
+              <div className="card-body">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.
+              </div>
+            </div>
           </div>
-          <div className="card-body">
-            Located in Bagan and with Manuha Temple reachable within 1.7 miles,
-            Ostello Bello Bagan Pool provides a tour desk, rooms, a bar, free
-            WiFi throughout the property and a shared lounge. Featuring family
-            rooms, this property also provides guests with a terrace. The hostel
-            has an outdoor swimming pool, evening entertainment and a 24-hour
-            front desk. All rooms feature a private bathroom, a hairdryer and
-            bed linen. Guests at the hostel can enjoy a buffet or an Asian
-            breakfast. Gu Byauk Gyi Myin Kabar is 3.2 km from Ostello Bello
-            Bagan Pool, while Dhammayazika Pagoda is 3.7 km from the property.
-            The nearest airport is Nyaung U Airport, 10 km from the
-            accommodation. Ostello Bello Bagan Pool has been welcoming
-            Booking.com guests since 1 Dec 2017 Hotel chain/brand: Ostello Bello
-            Distance in property description is calculated using © OpenStreetMap
+          <div className="col-lg-12">
+            <div className="card">
+              <div className="card-header bg-white">
+                <h5 className="mt-2"> Hotel Area Info</h5>
+              </div>
+              <div className="card-body">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. It has survived not only five centuries, but also the
+                leap into electronic typesetting, remaining essentially
+                unchanged. It was popularised in the 1960s with the release of
+                Letraset sheets containing Lorem Ipsum passages, and more
+                recently with desktop publishing software like Aldus PageMaker
+                including versions of Lorem Ipsum.
+              </div>
+            </div>
           </div>
         </div>
       </div>
