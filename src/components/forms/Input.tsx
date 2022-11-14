05132a214c16ec9815297e74341c721be9f86b2a
Phyo Htet Arkar <phyohtetarkar@gmail.com>
2022-11-14 23:26:15 +0630

    add autocomplete select

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,4 +1,11 @@
-import { ChangeEvent, FocusEvent, LegacyRef, MouseEvent } from "react";
+import {
+  ChangeEvent,
+  CSSProperties,
+  FocusEvent,
+  forwardRef,
+  HTMLInputTypeAttribute,
+  MouseEvent
+} from "react";
 import { formControlHeight } from "../../common/app.config";
 
 type OnChange<E> = (e: ChangeEvent<E>) => void;
@@ -7,71 +14,81 @@ type OnClick<E> = (e: MouseEvent<E>) => void;
 
 export interface InputProps<ElementType> {
   label?: string;
-  ref?: LegacyRef<HTMLInputElement>;
   id?: string;
   name?: string;
-  type?: string;
+  type?: HTMLInputTypeAttribute;
   placeholder?: string;
   value?: string | number | readonly string[];
   defaultValue?: string | number | readonly string[];
   error?: string;
   className?: string;
+  style?: CSSProperties;
   height?: number;
-  readonly?: boolean;
   disabled?: boolean;
+  readonly?: boolean;
+  autoFocus?: boolean;
   onChange?: OnChange<ElementType>;
   onBlur?: OnBlur<ElementType>;
   onClick?: OnClick<ElementType>;
 }
 
-function Input({
-  label,
-  ref,
-  id,
-  type,
-  name,
-  placeholder,
-  disabled,
-  readonly,
-  value,
-  defaultValue,
-  onChange,
-  onBlur,
-  onClick,
-  error,
-  className,
-  height = formControlHeight
-}: InputProps<HTMLInputElement>) {
-  return (
-    <>
-      {label && (
-        <label htmlFor={id} className="form-label">
-          {label}
-        </label>
-      )}
-      <input
-        ref={ref}
-        id={id}
-        type={type}
-        name={name}
-        className={`form-control px-3 ${error ? "is-invalid" : ""} ${
-          className ?? ""
-        }`}
-        placeholder={placeholder}
-        disabled={disabled}
-        value={value}
-        defaultValue={defaultValue}
-        onClick={onClick}
-        onChange={onChange}
-        onBlur={onBlur}
-        style={{
-          height: height
-        }}
-        readOnly={readonly}
-      />
-      {error && <div className="invalid-feedback">{error}</div>}
-    </>
-  );
-}
+const Input = forwardRef<HTMLInputElement, InputProps<HTMLInputElement>>(
+  (props, ref) => {
+    const {
+      label,
+      id,
+      type,
+      name,
+      placeholder,
+      disabled,
+      readonly,
+      autoFocus,
+      value,
+      defaultValue,
+      onChange,
+      onBlur,
+      onClick,
+      error,
+      className,
+      style,
+      height = formControlHeight
+    } = props;
+
+    return (
+      <>
+        {label && (
+          <label htmlFor={id} className="form-label">
+            {label}
+          </label>
+        )}
+        <input
+          ref={ref}
+          id={id}
+          type={type}
+          name={name}
+          className={`form-control px-3 ${error ? "is-invalid" : ""} ${
+            className ?? ""
+          }`}
+          placeholder={placeholder}
+          disabled={disabled}
+          readOnly={readonly}
+          autoFocus={autoFocus}
+          value={value}
+          defaultValue={defaultValue}
+          onChange={onChange}
+          onBlur={onBlur}
+          onClick={onClick}
+          style={{
+            ...style,
+            height: height
+          }}
+        />
+        {error && <div className="invalid-feedback">{error}</div>}
+      </>
+    );
+  }
+);
+
+Input.displayName = "Input";
 
 export default Input;
