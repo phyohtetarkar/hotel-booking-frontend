91b712d6a4f19cb76cdd8cf0aeec2e3bcbf503b6
MinnKhantKyaww <minkhant199701@gmail.com>
2022-11-20 21:47:48 +0630

    Update Orders UI

--- src/pages/profile/booking/[bookingId]/index.tsx
+++ src/pages/profile/booking/[bookingId]/index.tsx
@@ -1,7 +1,33 @@
 import Link from "next/link";
 import { formatTimestamp } from "../../../../common/utils";
 
+function BookingRow() {
+  return (
+    <tr>
+      <td className="ps-3 ps-lg-4 w-100">
+        <div className="d-flex">
+          <span className="text-primary"><Link className="text-dark text-decoration-none" href="#">Deluxe Room</Link></span>
+        </div>
+      </td>
+      <td>
+        <span className="text-nowrap fw-light">2</span>
+      </td>
+      <td>
+        <span className="text-nowrap fw-light">1 Double - no extra</span>
+      </td>
+      <td>
+        <span className="text-nowrap fw-light">2</span>
+      </td>
+      <td>
+        <span className="text-nowrap fw-light">MMK&nbsp;200,000</span>
+      </td>
+    </tr>
+  );
+}
+
 function BookingDetail() {
+  const list = [1, 2, 3];
+
   return (
     <div className="pb-5">
       <div className="bg-primary">
@@ -35,21 +61,24 @@ function BookingDetail() {
           <div className=" col-lg-12">
             <div className="card mb-3">
               <div className="card-header py-3 bg-white">
-                <div className="row">
+                <div className="row align-items-center">
                   <div className="col d-flex">
-                    <span className="fw-semibold h5 my-auto">
-                      Reserve ID: 20001
-                    </span>
+                    <div className="vstack gap-1">
+                      <Link className="fw-semibold h5 my-auto" href={"/profile/hotels/1"}>
+                        Bagan View Hotel
+                      </Link>
+                      <small className="text-muted text-wrap">#ID 20001</small>
+                    </div>
                   </div>
                   <div className="col-auto">
-                    <Link className="btn btn-sm btn-outline-danger" href={"#"}>
+                    <Link className="btn btn-sm btn-outline-danger align-self-center" href={"#"}>
                       Cancel booking
                     </Link>
                   </div>
                 </div>
               </div>
-              <div className="card-body">
-                <div className="row gx-2 gy-3">
+              <div className="card-body p-0">
+                <div className="row gx-2 gy-3 mb-4 p-3">
                   <div className="col-md-5">
                     <h6 className="fw-bold">Customer info</h6>
                     <div className="vstack text-dark small">
@@ -60,17 +89,10 @@ function BookingDetail() {
                     </div>
                   </div>
                   <div className="col-md-4">
-                    <h6 className="fw-bold">Reserve info</h6>
-                    <div>Room type: Delux Room</div>
-                    <div>Bed: 1 Double - no extra</div>
-                    <div>Sleeps: Adult 2, Children 1</div>
-                    <div>Rooms: 1</div>
-                    <div>Day: 2</div>
-                    <div>Subtotal: 30,000ks</div>
-                    <div className="text-danger">Discounts: -0ks</div>
-                    <div className="text-success fw-semibold">
-                      Total Price: 33,000ks
-                    </div>
+                    <h6 className="fw-bold">Additional info</h6>
+                    <div>Floor preference: 4 floor</div>
+                    <div>Check in: {formatTimestamp(Date.now(), true)}</div>
+                    <div>Special request: Something else...</div>
                   </div>
                   <div className="col-md-3">
                     <h6 className="fw-bold">Status</h6>
@@ -79,8 +101,72 @@ function BookingDetail() {
                     </div>
                   </div>
                 </div>
-                <hr className="dark-gray" />
-                <div className="row row-cols-1 row-cols-md-3 g-3"></div>
+                <div className="row mb-3">
+                  <div className="col-lg-12">
+                    {/* <div className="card">
+                      <div className="card-body p-0"> */}
+                        <div className="table-responsive">
+                          <table className="table bg-white align-middle">
+                            <thead className="text-nowrap align-middle" style={{backgroundColor: "#f9f9f9"}}>
+                              <tr style={{ height: 50 }}>
+                                <th
+                                  className="ps-3 ps-lg-4 fw-normal text-muted"
+                                  style={{ minWidth: 200 }}
+                                >
+                                  Room Type
+                                </th>
+                                <th
+                                  className="fw-normal text-muted"
+                                  style={{ minWidth: 150 }}
+                                >
+                                  Sleeps
+                                </th>
+                                <th
+                                  className="fw-normal text-muted"
+                                  style={{ minWidth: 200 }}
+                                >
+                                  Bed
+                                </th>
+                                <th
+                                  className="fw-normal text-muted"
+                                  style={{ minWidth: 100 }}
+                                >
+                                  Day
+                                </th>
+                                <th
+                                  className="fw-normal text-muted"
+                                  style={{ minWidth: 100 }}
+                                >
+                                  Price Per Room
+                                </th>
+                              </tr>
+                            </thead>
+                            <tbody className="border-top-0">
+                              {list.map((i) => (
+                                <BookingRow key={i} />
+                              ))}
+                              <tr>
+                                <td colSpan={3}></td>
+                                <td className="fw-semibold">Subtotal</td>
+                                <td className="">MMK&nbsp;200,000</td>
+                              </tr>
+                              <tr>
+                                <td colSpan={3}></td>
+                                <td className="fw-semibold">Discounts</td>
+                                <td className="text-danger">-0ks</td>
+                              </tr>
+                              <tr>
+                                <td colSpan={3}></td>
+                                <td className="fw-semibold">Total Price</td>
+                                <td className="text-success"><h5>MMK&nbsp;203,000</h5></td>
+                              </tr>
+                            </tbody>
+                          </table>
+                        {/* </div>
+                      </div> */}
+                    </div>
+                  </div>
+                </div>
               </div>
               <div className="card-footer small border-0 py-3 text-muted">
                 {formatTimestamp(Date.now(), true)}
--- src/pages/profile/booking/index.tsx
+++ src/pages/profile/booking/index.tsx
@@ -5,117 +5,117 @@ import { Input, Select } from "../../../components/forms";
 import Pagination from "../../../components/Pagination";
 
 function BookingCard() {
-    return (
-      <div className="card mb-3">
-        <div className="card-header py-3 bg-white">
-          <div className="row">
-            <div className="col d-flex">
-              <span className="fw-semibold h5 my-auto">Reserve ID: 20001</span>
+  return (
+    <div className="card mb-3">
+      <div className="card-header py-3 bg-white">
+        <div className="row">
+          <div className="col d-flex">
+            <span className="fw-semibold h5 my-auto">Booking ID: 20001</span>
+          </div>
+          <div className="col-auto">
+            <Link
+              className="btn btn-sm btn-outline-primary"
+              href={"/profile/booking/1"}
+            >
+              View detail
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="card-body">
+        <div className="row gx-2 gy-3">
+          <div className="col-md-5">
+            <h6 className="fw-bold">Customer info</h6>
+            <div className="vstack text-dark small">
+              <span>Name: Mobile Com</span>
+              <span>Phone: +95911223344</span>
+              <span>Address: No. 26, Pyay Street, Hlaing Township</span>
+              <span>Yangon, Myanmar</span>
             </div>
-            <div className="col-auto">
-              <Link className="btn btn-sm btn-outline-primary" href={"/profile/booking/1"}>
-                View detail
-              </Link>
+          </div>
+          <div className="col-md-4">
+            <h6 className="fw-bold">Payment info</h6>
+            <div>Subtotal: MMK&nbsp;30,000</div>
+            <div className="text-danger">Discounts: MMK&nbsp;-0</div>
+            <div className="text-success fw-semibold">
+              Total Price: MMK&nbsp;33,000
+            </div>
+          </div>
+          <div className="col-md-3">
+            <h6 className="fw-bold">Status</h6>
+            <div className="text-warning">
+              <span className="fw-semibold">Pending</span>
             </div>
           </div>
         </div>
-        <div className="card-body">
-          <div className="row gx-2 gy-3">
-            <div className="col-md-5">
-              <h6 className="fw-bold">Customer info</h6>
-              <div className="vstack text-dark small">
-                <span>Name: Mobile Com</span>
-                <span>Phone: +95911223344</span>
-                <span>Address: No. 26, Pyay Street, Hlaing Township</span>
-                <span>Yangon, Myanmar</span>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <h6 className="fw-bold">Reserve info</h6>
-              <div>Room type: Delux Room</div>
-              <div>Bed: 1 Double - no extra</div>
-              <div>Sleeps: Adult 2, Children 1</div>
-              <div>Rooms: 1</div>
-              <div>Day: 2</div>
-              <div>Subtotal: 30,000ks</div>
-              <div className="text-danger">Discounts: -0ks</div>
-              <div className="text-success fw-semibold">
-                Total Price: 33,000ks
-              </div>
-            </div>
-            <div className="col-md-3">
-              <h6 className="fw-bold">Status</h6>
-              <div className="text-warning">
-                <span className="fw-semibold">Pending</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card-footer small border-0 py-3 text-muted">
-          {formatTimestamp(Date.now(), true)}
-        </div>
-        </div>
-    );
-  }
+      </div>
+      <div className="card-footer small border-0 py-3 text-muted">
+        {formatTimestamp(Date.now(), true)}
+      </div>
+    </div>
+  );
+}
 
 function MyBooking() {
-    const list = [1, 2];
+  const list = [1, 2];
 
-    return (
-        <div>
-          <div className="bg-primary">
-            <div className="container">
-              <div className="py-4 py-lg-5">
-                <h1 className="text-light text-center text-lg-start">
-                  My Booking
-                </h1>
+  return (
+    <div>
+      <div className="bg-primary">
+        <div className="container">
+          <div className="py-4 py-lg-5">
+            <h1 className="text-light text-center text-lg-start">My Booking</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="container py-4">
+        <div className="row g-4">
+          <div className="col-lg-4 col-xl-3">
+            <AccountMenu />
+          </div>
+          <div className="col-lg-8 col-xl-9">
+            <div
+              className="card mb-3 p-3"
+            >
+              <div className="row">
+                <div className="col">
+                  <Input
+                    id="searchinput"
+                    name="search"
+                    type="text"
+                    placeholder="Search your booking"
+                  />
+                </div>
+                <div className="col-auto">
+                  <Select>
+                    <option value="">All Status</option>
+                    <option value="">Pending</option>
+                    <option value="">Suspended</option>
+                    <option value="">Deleted</option>
+                  </Select>
+                </div>
+                <div className="col-auto">
+                  <Link
+                    href="#"
+                    className="ms-auto btn btn-primary h-100 hstack"
+                  >
+                    Search
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-    
-          <div className="container py-4">
-            <div className="row g-4">
-              <div className="col-lg-4 col-xl-3">
-                <AccountMenu />
-              </div>
-              <div className="col-lg-8 col-xl-9">
-                <div className="card mb-3 p-3">
-                  <div className="row">
-                    <div className="col">
-                      <Input
-                        id="searchinput"
-                        name="search"
-                        type="text"
-                        placeholder="Search your booking"
-                      />
-                    </div>
-                    <div className="col-auto">
-                      <Select>
-                        <option value="">All Status</option>
-                        <option value="">Pending</option>
-                        <option value="">Suspended</option>
-                        <option value="">Deleted</option>
-                      </Select>
-                    </div>
-                    <div className="col-auto">
-                      <Link href="#" className="ms-auto btn btn-primary h-100 hstack">
-                          Search
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-                {list.map((i) => (
-                  <BookingCard key={i}/>
-                ))}
-                <div className="d-flex justify-content-end pt-3">
-                  <Pagination hasPrev={true} hasNext={true} />
-                </div>
-                
-              </div>
+            {list.map((i) => (
+              <BookingCard key={i} />
+            ))}
+            <div className="d-flex justify-content-end pt-3">
+              <Pagination hasPrev={true} hasNext={true} />
             </div>
           </div>
         </div>
-      );
+      </div>
+    </div>
+  );
 }
 
 export default MyBooking;
